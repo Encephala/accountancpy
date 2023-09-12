870e86376a219527ac7139739b1d4c009131a895
Encephala <jonathan_rietveld@hotmail.com>
2023-09-12 21:40:25 +0200

    Fix URL from hardcoded to template

--- app/ledgers/urls.py
+++ app/ledgers/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "ledgers"
+
 urlpatterns = [
-    path("", views.overview, name="ledgers-overview"),
+    path("", views.overview, name="overview"),
 ]
