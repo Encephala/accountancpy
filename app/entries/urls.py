a9f7b3ec7d108749fb9922ce604d41ef76dde716
Encephala <jonathan_rietveld@hotmail.com>
2023-09-24 18:33:14 +0200

    Fix wrong view for Details and hardcoded `entry_id`

--- app/entries/urls.py
+++ app/entries/urls.py
@@ -7,7 +7,7 @@ app_name = "entries"
 urlpatterns = [
     path("", views.overview, name = "overview"),
     path("list/", views.EntryList.as_view(), name = "entry_list"),
-    path("<entry_id>/", views.EntryList.as_view(), name = "details"),
     path("by-entry/<entry_id>/", views.EntryRowByEntry.as_view(), name = "entry_rows"),
     path("by-ledger/<ledger_id>/", views.EntryRowByLedger.as_view(), name = "ledger_rows"),
+    path("<entry_id>/", views.EntryDetails.as_view(), name = "details"),
 ]
