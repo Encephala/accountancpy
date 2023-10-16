0cab1d6853f0ebfd3a428a09c4384cb14cefd6c0
Encephala <jonathan_rietveld@hotmail.com>
2023-10-16 22:08:38 +0200

    change: Slight adjustment to HTMX endpoint path for consistency

--- app/entries/urls.py
+++ app/entries/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path("rows-by-entry/<pk>/", views.EntryRowByEntry.as_view(), name = "entry_rows"),
     path("rows-by-ledger/<pk>/", views.EntryRowByLedger.as_view(), name = "ledger_rows"),
     path("rows-by-account/<pk>/", views.EntryRowByAccount.as_view(), name = "account_rows"),
-    path("entry-by-journal/<pk>/", views.EntryByJournal.as_view(), name = "journal_rows"),
+    path("by-journal/<pk>/", views.EntryByJournal.as_view(), name = "journal_rows"),
     path("<pk>/", views.EntryDetails.as_view(), name = "details"),
 ]
